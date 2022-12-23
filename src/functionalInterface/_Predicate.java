d5a6340c3fadcf1b2f9bc762c4990744206ab70b
Liew Chen Tzer <liewct115@gmail.com>
2022-12-23 11:47:36 +0800

    [UPDATE] 1) Predicate

---
+++ src/functionalInterface/_Predicate.java
@@ -0,0 +1,31 @@
+package functionalInterface;
+
+import java.util.function.Predicate;
+
+public class _Predicate {
+
+    public static void main(String[] args) {
+        System.out.println("Normal Function");
+        System.out.println(isPhoneNumberValid("0100000000"));
+        System.out.println(isPhoneNumberValid("0200000000"));
+
+        System.out.println("\nPredicate");
+        System.out.println(isPhoneNumberValidPredicate.test("0100000000"));
+        System.out.println(isPhoneNumberValidPredicate.test("0200000000"));
+
+        System.out.println("\nUse 2 Predicate");
+        System.out.println("AND");
+        System.out.println(isPhoneNumberValidPredicate.and(containsNumber1).test("0100000000"));
+        System.out.println("OR");
+        System.out.println(isPhoneNumberValidPredicate.or(containsNumber1).test("0100000000"));
+
+    }
+
+    static boolean isPhoneNumberValid (String phoneNumber){
+        return phoneNumber.startsWith("01") && phoneNumber.length() == 10;
+    }
+
+    static Predicate<String> isPhoneNumberValidPredicate = phoneNumber -> phoneNumber.startsWith("01") && phoneNumber.length() == 10;
+
+    static Predicate<String> containsNumber1 = phoneNumber -> phoneNumber.contains("3");
+}

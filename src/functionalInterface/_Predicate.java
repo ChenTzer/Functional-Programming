6ecd6ff3077fe97da7fc166878e14b2211a7fe99
Liew Chen Tzer <liewct115@gmail.com>
2022-12-23 12:03:54 +0800

    [UPDATE] 1) BiPredicate 2) Created README File

--- src/functionalInterface/_Predicate.java
+++ src/functionalInterface/_Predicate.java
@@ -1,5 +1,6 @@
 package functionalInterface;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class _Predicate {
@@ -19,6 +20,9 @@ public class _Predicate {
         System.out.println("OR");
         System.out.println(isPhoneNumberValidPredicate.or(containsNumber1).test("0100000000"));
 
+        System.out.println("\nBiPredicate");
+        System.out.println(isPhoneNumberPlusVerification.test("0100000000", 0001));
+
     }
 
     static boolean isPhoneNumberValid (String phoneNumber){
@@ -28,4 +32,6 @@ public class _Predicate {
     static Predicate<String> isPhoneNumberValidPredicate = phoneNumber -> phoneNumber.startsWith("01") && phoneNumber.length() == 10;
 
     static Predicate<String> containsNumber1 = phoneNumber -> phoneNumber.contains("3");
+
+    static BiPredicate<String, Number> isPhoneNumberPlusVerification = (phoneNumber, verificationCode) -> phoneNumber.startsWith("01") && verificationCode.equals(0000);
 }

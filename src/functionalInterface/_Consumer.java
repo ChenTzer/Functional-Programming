66a33ccb647027cec9b9dd555759832fe30aa470
Liew Chen Tzer <liewct115@gmail.com>
2022-12-23 11:33:34 +0800

    [UPDATE] 1) BiConsumer

--- src/functionalInterface/_Consumer.java
+++ src/functionalInterface/_Consumer.java
@@ -1,22 +1,39 @@
 package functionalInterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-
-import static functionalInterface._Consumer.Customer.greetCustomer;
 
 public class _Consumer {
 
     public static void main(String[] args) {
         //Normal Java Function
         Customer maria = new Customer("Maria", "9999");
+        System.out.println("Normal Java Function");
         greetCustomer(maria);
+        System.out.println();
+        greetCustomer2(maria, false);
+        System.out.println();
 
         //Consumer Functional Interface
+        System.out.println("Consumer");
         greetCustomerConsumer.accept(maria);
+        System.out.println("\nBiConsumer");
+        greetCustomerConsumerV2.accept(maria, false);
     }
 
     //Consumer
     static Consumer<Customer> greetCustomerConsumer = customer -> System.out.println("Hello " +customer.customerName +", thanks for registering phone number " +customer.customerPhoneNumber);
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = (customer, showPhoneNumber) -> System.out.println("Hello " +customer.customerName +", thanks for registering phone number " +(showPhoneNumber ? customer.customerPhoneNumber : "****"));
+
+    //Normal Java Function
+    static void greetCustomer(Customer customer){
+        System.out.println("Hello " +customer.customerName +", thanks for registering phone number " +customer.customerPhoneNumber);
+    }
+
+    static void greetCustomer2(Customer customer, boolean showPhoneNumber){
+        System.out.println("Hello " +customer.customerName +", thanks for registering phone number " +(showPhoneNumber ? customer.customerPhoneNumber : "****"));
+    }
 
     static class Customer{
         private final String customerName;
@@ -26,10 +43,7 @@ public class _Consumer {
             this.customerPhoneNumber = customerPhoneNumber;
         }
 
-        //Normal Java Function
-        static void greetCustomer(Customer customer){
-            System.out.println("Hello " +customer.customerName +", thanks for registering phone number " +customer.customerPhoneNumber);
-        }
+
 
     }
 }

cafe953ce67639c5f666c71d3f4b3890dac8f49c
Liew Chen Tzer <liewct115@gmail.com>
2022-12-25 01:13:45 +0800

    [UPDATE] 1) With Combinator Pattern 2) Added YouTube Link to txt file

--- src/designPattern/combinatorPattern/Main.java
+++ src/designPattern/combinatorPattern/Main.java
@@ -2,12 +2,27 @@ package designPattern.combinatorPattern;
 
 import java.time.LocalDate;
 
+import static designPattern.combinatorPattern.CustomerRegistrationValidator.*;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer(
-                "Alice", "alice@gmail.com","0100000000", LocalDate.of(2000, 11 , 05));
+                "Alice", "alice@gmail.com","0100000000", LocalDate.of(2000, 11 , 5));
 
-        CustomerValidatorService validatorService = new CustomerValidatorService();
-        validatorService.isValid(customer);
+        //Normal Method
+//        CustomerValidatorService validatorService = new CustomerValidatorService();
+//        validatorService.isValid(customer);
+
+        //Combinator Pattern
+        ValidationResult result = isEmailValid()
+                                    .and(isPhoneNumberValid())
+                                    .and(isAnAdult())
+                                    .apply(customer);
+
+        System.out.println(result);
+
+        if (result != ValidationResult.SUCCESS){
+            throw new IllegalStateException(result.name());
+        }
     }
 }

df8835731aecf68558aa060ddedb7f1f0c504497
Liew Chen Tzer <liewct115@gmail.com>
2022-12-30 15:18:27 +0800

    [UPDATE] 1) Optionals (More Details)

--- src/optionals/Main.java
+++ src/optionals/Main.java
@@ -10,6 +10,69 @@ public class Main {
                     () -> {
                     System.out.println("nice");
                     });
+
+        // 12/30/2022
+        Optional<String> hello = Optional.ofNullable("hello");
+
+        System.out.println(hello.isPresent());
+        System.out.println(hello.isEmpty());
+
+        //if null then assign this value
+        String orElse = hello
+                .orElse("world");
+
+        //map = transformation on the value
+        String orElse2 = hello
+                .map(String::toUpperCase)
+                .orElse("world");
+
+        //orElseGet = can run Supplier
+        String orElse3 = hello
+                .map(String::toUpperCase)
+                .orElseGet(()->{
+                    // ... extra computation to retrieve the value
+                    return "world";
+        });
+
+        //or = can return optional
+
+        //orElseThrow = can throw exception
+        String orElse4 = hello
+                .map(String::toUpperCase)
+                .orElseThrow(IllegalStateException::new);
+
+        //ifPresent = take the value and do somthing
+        hello.ifPresent(word -> {
+            System.out.println(word);
+        });
+
+        //ifPresentOrElse
+        hello.ifPresentOrElse(System.out::println,
+                () -> System.out.println("world"));
+
+        System.out.println(orElse);
+        System.out.println();
+
+        Person person = new Person("James", null);
+        System.out.println(person.getEmail().map(String::toLowerCase).orElse("No Email"));
+    }
+
+    static class Person{
+        private final String name;
+        private final String email;
+
+        public Person(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Optional<String> getEmail() {
+            return Optional.ofNullable(email);
+        }
     }
 
 }
